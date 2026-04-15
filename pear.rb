3a78ac58ce7ff41a6a86aeb08d7de01419dd72e4
helson-lin <84565447+helson-lin@users.noreply.github.com>
2026-04-15 12:35:34 +0800

    Update pear.rb

--- pear.rb
+++ pear.rb
@@ -6,7 +6,7 @@ class Pear < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/NoTermTm/pear/releases/download/v0.0.2-test/pear-macos-aarch64.tar.gz"
-      sha256 "e7f124e03c231a1e33fe9e9cddb89f8d83cc9ed25c60629f2ff7ccfa5123cfa6"
+      sha256 "86ef9acc728d2a8dcba53ffcc31d004d6c464c0e6c7cef866e509ef4f1b43c7c"
     else
       url "https://github.com/NoTermTm/pear/releases/download/v0.0.2-test/pear-macos-x86_64.tar.gz"
       sha256 "220d1ed79026925e7e0802fed2186bda02126d18b46aad064f58b838fb99c138"
